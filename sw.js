85e699b94c4b5e75c1f1b74d96b768be87eaf374
richs-beep <richsulpak@gmail.com>
2026-02-09 14:53:44 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cake-3d-v2.5';
+const CACHE_NAME = 'cake-3d-v2.6';
 const ASSETS = [
   './',
   './index.html',
@@ -43,5 +43,6 @@ self.addEventListener('fetch', (e) => {
       })
   );
 });
+
 
 
